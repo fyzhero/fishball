04012eeddd78640ae5537690f8b4f58ad602c1ad
fyzhero <fyzhero@gmail.com>
2016-09-27 13:55:23 +0800

    first commit

---
+++ server_main.js
@@ -0,0 +1,54 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var url = require('url');
+
+app.get('/', function(request, response){
+	response.sendFile('/home/ifan/node_server/chat_socket/index.html');
+	var path = url.parse(request.url).pathname;
+	console.log(path);
+});
+
+app.get('/private_get', function(req, res){
+	//room_id = req.query.room_id;
+	//console.log("room_id:%s", room_id);
+	//res.send("server get room_id success!");
+	res.sendFile('/home/ifan/node_server/chat_socket/index.html');
+});
+
+io.on('connection', function(socket){
+
+	console.log('a user connected');
+
+	socket.on('disconnect', function(){
+		console.log('user disconnected');	
+	});
+
+	// first enter a channel
+	socket.on('create', function(room_id){
+		socket.join(room_id);
+		console.log("server socket join room ", room_id);
+	});
+
+	// exit a channel
+	socket.on('exit', function(room_id){
+		socket.leave(room_id);
+		console.log("server socket leave room ", room_id);
+	});
+
+	socket.on('chat message', function(msg){
+    	console.log('message: ' + msg);
+	    
+		var msgobj = JSON.parse(msg);
+		io.to(msgobj['room_id']).emit('chat message', msgobj['msg']);
+	});
+
+	
+});
+
+// server start
+var port = 3003
+http.listen(port, function(){
+	console.log('listening on *:' + port);	
+});
